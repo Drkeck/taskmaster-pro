dd889f646d49f16b475534567dec7d4a93f30e25
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2020-04-08 05:54:40 -0700

    added drag and drop as well as drop to delete features requested

--- assets/js/script.js
+++ assets/js/script.js
@@ -149,6 +149,58 @@ $(".list-group").on("blur", "input[type='text']", function() {
   $(this).replaceWith(taskSpan);
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  
+  update: function(event) {
+    var tempArr = [];
+
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    console.log(tempArr);
+
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+})
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
